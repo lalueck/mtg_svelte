e453e028311dc029b6bd772e4a1155b370c655ae
linus <linuslueck@gmx.net>
2022-04-07 16:00:49 +0200

    added accordeon and centering

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,9 @@
 	let cardPromise = getCard();
 </script>
 
+
+<center>
+
 {#await cardPromise}
 	<p>...waiting</p>
 {:then object}
@@ -19,65 +22,80 @@
 	<img src="{object.cards[0].imageUrl}" alt="image of the card"/>
 
 	<!-- ausklappbar -->
-	<p>manaCost</p><IsPresentP value={object.cards[0].manaCost}/>
-	<p>cmc</p><IsPresentP value={object.cards[0].cmc}/>
-	<p>colors</p><IsPresentP value={object.cards[0].colors}/>
-	<p>colorIdentity</p><IsPresentP value={object.cards[0].colorIdentity}/>
-	<p>type</p><IsPresentP value={object.cards[0].type}/>
-	<p>supertypes</p><IsPresentP value={object.cards[0].supertypes}/>
-	<p>types</p><IsPresentP value={object.cards[0].types}/>
-	<p>subtypes</p><IsPresentP value={object.cards[0].subtypes}/>
-	<p>rarity</p><IsPresentP value={object.cards[0].rarity}/>
-	<p>set</p><IsPresentP value={object.cards[0].set}/>
 	<p>text</p><IsPresentP value={object.cards[0].text}/>
-	<p>artist</p><IsPresentP value={object.cards[0].artist}/>
-	<p>number</p><IsPresentP value={object.cards[0].number}/>
 	<p>power</p><IsPresentP value={object.cards[0].power}/>
-	<p>tougheness</p><IsPresentP value={object.cards[0].tougheness}/>
-	<p>multiverseid</p><IsPresentP value={object.cards[0].multiverseid}/>
-	<p>rulings</p><IsPresentP value={object.cards[0].rulings}/>
-	<p>originalText</p><IsPresentP value={object.cards[0].originalText}/>
-	<p>originalType</p><IsPresentP value={object.cards[0].originalType}/>
-	<p>id</p><IsPresentP value={object.cards[0].id}/>
+	<p>toughness</p><IsPresentP value={object.cards[0].toughness}/>
+
+
+
+
+
 
 	<div class="accordion" id="accordionPanelsStayOpenExample">
 		<div class="accordion-item">
 		  <h2 class="accordion-header" id="panelsStayOpen-headingOne">
 			<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-			  Accordion Item #1
+			  Mana and colours
 			</button>
 		  </h2>
-		  <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+		  <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
 			<div class="accordion-body">
-			  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+				<p>manaCost</p><IsPresentP value={object.cards[0].manaCost}/>
+				<p>cmc</p><IsPresentP value={object.cards[0].cmc}/>
+				<p>colors</p><IsPresentP value={object.cards[0].colors}/>
+				<p>colorIdentity</p><IsPresentP value={object.cards[0].colorIdentity}/>
 			</div>
 		  </div>
 		</div>
 		<div class="accordion-item">
 		  <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
 			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-			  Accordion Item #2
+			  Type, Types and more
 			</button>
 		  </h2>
 		  <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
 			<div class="accordion-body">
-			  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+				<p>type</p><IsPresentP value={object.cards[0].type}/>
+				<p>supertypes</p><IsPresentP value={object.cards[0].supertypes}/>
+				<p>types</p><IsPresentP value={object.cards[0].types}/>
+				<p>subtypes</p><IsPresentP value={object.cards[0].subtypes}/>
 			</div>
 		  </div>
 		</div>
 		<div class="accordion-item">
 		  <h2 class="accordion-header" id="panelsStayOpen-headingThree">
 			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-			  Accordion Item #3
+			  Misc info
 			</button>
 		  </h2>
 		  <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
 			<div class="accordion-body">
-			  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+				<p>rarity</p><IsPresentP value={object.cards[0].rarity}/>
+				<p>set</p><IsPresentP value={object.cards[0].set}/>
+				<p>artist</p><IsPresentP value={object.cards[0].artist}/>
+				<p>number</p><IsPresentP value={object.cards[0].number}/>
+				<p>multiverseid</p><IsPresentP value={object.cards[0].multiverseid}/>
+				<p>id</p><IsPresentP value={object.cards[0].id}/>
 			</div>
 		  </div>
 		</div>
+		<div class="accordion-item">
+			<h2 class="accordion-header" id="panelsStayOpen-headingFour">
+			  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
+				Historic info
+			  </button>
+			</h2>
+			<div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
+			  <div class="accordion-body">
+				<p>rulings</p><IsPresentP value={object.cards[0].rulings}/>
+				<p>originalText</p><IsPresentP value={object.cards[0].originalText}/>
+				<p>originalType</p><IsPresentP value={object.cards[0].originalType}/>
+			
+			  </div>
+			</div>
+		  </div>
 	  </div>
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
+</center>
