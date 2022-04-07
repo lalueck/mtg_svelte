c8c31118fe7aa218f829dbf8a0d7a452aeca7088
linus <linuslueck@gmx.net>
2022-04-07 13:42:00 +0200

    changed static card to random card + image of the card

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	const getCard = async () => {
-		var response = await fetch('https://api.magicthegathering.io/v1/cards/386616');
+		var response = await fetch('https://api.magicthegathering.io/v1/cards?random=true&pageSize=01&contains=imageUrl&layout=normal');
 		var result = await response.json();
 		return result;
 	}
@@ -11,7 +11,9 @@
 {#await cardPromise}
 	<p>...waiting</p>
 {:then object}
-	<p>{JSON.stringify(object.card.name)}</p>
+	<p>{JSON.stringify(object.cards[0].name)}</p>
+	<!-- svelte-ignore a11y-img-redundant-alt -->
+	<img src="{object.cards[0].imageUrl}" alt="image of the card"/>
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
