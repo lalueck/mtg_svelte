5bac4efbc352c4aa4a1a92adc5395f87624a4de9
linus <linuslueck@gmx.net>
2022-04-07 12:08:59 +0200

    Initial commit, website returns static name as of now

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	const getCard = async () => {
+		var response = await fetch('https://api.magicthegathering.io/v1/cards/386616');
+		var result = await response.json();
+		return result;
+	}
+
+	let cardPromise = getCard();
+</script>
+
+{#await cardPromise}
+	<p>...waiting</p>
+{:then object}
+	<p>{JSON.stringify(object.card.name)}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
